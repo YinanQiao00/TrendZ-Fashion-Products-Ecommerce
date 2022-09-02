3f49a06899a5a912add68dc8801a6917faa074ee
YinanQiao00 <yiqiao@ucsd.edu>
2022-09-01 23:14:39 -0700

    final

--- mainJs.js
+++ mainJs.js
@@ -2,6 +2,9 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
+var mainImage = document.getElementById('mainImage');
+var smallImage = document.getElementsByClassName('smallImages');
+
 if(bar){
     bar.addEventListener('click', ()=>{
         nav.classList.add('active');
@@ -11,4 +14,20 @@ if(close){
     close.addEventListener('click', ()=>{
         nav.classList.remove('active');
     })
+}
+
+smallImage[0].onclick = function (){
+    mainImage.src = smallImage[0].src;
+}
+
+smallImage[1].onclick = function (){
+    mainImage.src = smallImage[1].src;
+}
+
+smallImage[2].onclick = function (){
+    mainImage.src = smallImage[2].src;
+}
+
+smallImage[3].onclick = function (){
+    mainImage.src = smallImage[3].src;
 }
